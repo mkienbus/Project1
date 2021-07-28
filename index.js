719f719c2a77f675d1428da60e6228247ec9f870
Michael Kienbsuch <mkienbus@gmail.com>
2021-07-28 16:43:55 -0600

    fetches working

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 document.addEventListener("DOMContentLoaded", function(){
     console.log("the DOM has loaded");
-    //enable search function
+    displayAll();
 })
 
 //DOM grabs
-let searchForm = document.getElementById("cryptoQuery");
-let result = document.querySelector("body#searchResult ul li");
+let searchForm = document.getElementById("crypto-search");
+let result = document.querySelector("body#searchResult");
+let cryptoList = document.getElementById("cryptoList");
+let input = document.getElementById("cryptoQuery");
 
-searchForm.addEventListener("submit", function(e){
-    e.preventDefault();
-    let input = searchForm.nodeValue;
-    fetch(`https://api.coinpaprika.com/v1/${input}`)
+//function to display all cryptos upon DOMContentLoaded
+function displayAll(){
+    fetch(`https://api.coinpaprika.com/v1/coins`)
         .then(response => response.json())
         .then(function(data){
+            //console.log(data.slice(0, 100))
+            //for each iteration?
+            let sliced = data.slice(0, 100)
+            sliced.forEach((d) => {
+                cryptoList.innerHTML += `
+                    <li>
+                        ${d.name}
+                    </li>
+                `
+                //console.log(d.name)
+            })
             
+        }) 
+}
+
+
+//crypto search
+searchForm.addEventListener("submit", displayCrypto)
+
+function displayCrypto(e){
+    e.preventDefault()
+    fetch(`https://api.coinpaprika.com/v1/coins/${input.value}`)
+        .then(response => response.json())
+        .then(function(data){
+            // result.innerHTML = `
+            //     <ul>
+            //         <li>
+            //             ${data}
+            //         </li>
+            //     </ul>
+            //     `
+            console.log(data)
         })
 
     input.value = "";
-})
+}
